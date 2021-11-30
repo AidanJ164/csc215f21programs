a5c33e8d13469d1a96c799716f0778233582e931
Justice, Aidan <Aidan.Justice@mines.sdsmt.edu>
2021-11-30 12:54:52 -0700

    thpf - [ci skip] - set up constructor, destructor, alloc2d, and free2d

--- thpf/netPBM.cpp
+++ thpf/netPBM.cpp
@@ -1,1 +1,55 @@
 #include "netPBM.h"
+
+netPBM::netPBM()
+{
+
+}
+netPBM::~netPBM()
+{
+    free2d( redGray, rows );
+    free2d( green, rows );
+    free2d( blue, rows );
+}
+
+pixel** netPBM::alloc2d(int rows, int cols)
+{
+    int i;
+    pixel** arr = nullptr;
+
+    // Create array and check for success.
+    arr = new (nothrow) pixel* [rows];
+    if (arr == nullptr)
+    {
+        return nullptr;
+    }
+
+    // Create each column for the array and check for success.
+    for ( i = 0; i < rows; i++ )
+    {
+        arr[i] = new ( nothrow ) pixel[cols];
+        if (arr[i] == nullptr)
+        {
+            free2d( arr, i );
+            return nullptr;
+        }
+    }
+
+    return arr;
+}
+
+void netPBM::free2d( pixel** &arr, int rows )
+{
+    int i;
+
+    // Delete columns
+    for ( i = 0; i < rows; i++ )
+    {
+        delete[] arr[i];
+    }
+
+    // Delete the array
+    delete[] arr;
+}
+    
+    
+
